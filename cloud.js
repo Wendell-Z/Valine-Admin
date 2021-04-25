c8eb1b39f16b1bb19828bc9a5d93104a69087298
Wendell-Z <coder.wendell@gmail.com>
2021-04-25 23:31:34 +0800

    自唤醒任务修改-加日志

--- cloud.js
+++ cloud.js
@@ -79,7 +79,9 @@ AV.Cloud.define('self_wake', function(req) {
         },
         body: JSON.stringify(requestData)
     },function (error,response,body){
+        console.log(body);
         console.log('自唤醒任务执行成功，响应状态码为:', response && response.statusCode);
+        console.log(JSON.stringify(response));
     });
     // request(process.env.ADMIN_URL, function (error, response, body) {
     //     console.log('自唤醒任务执行成功，响应状态码为:', response && response.statusCode);
